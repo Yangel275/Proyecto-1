768ce58dc334879d0e47d51adc13c76525c12719
sergioob4 <144851074+sergioob4@users.noreply.github.com>
2024-02-20 20:16:02 -0400

    Calculos de la feoromonas
    Base del calculo de las feoromonas, falta algunas pruebas pero es la base

---
+++ src/Calculos/Prueba.java
@@ -0,0 +1,24 @@
+
+ 
+package Calculos;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Prueba {
+    
+    public static void main(String[] args){
+        Feoromonas f = new Feoromonas(0.5,0.4,0.3);
+        
+        double resultadobeta = f.actualizarFeromona(0.5,0.4,0.3);
+        
+        System.out.println("El resultado de prueba es "+resultadobeta);
+    }
+}
+
+
+/*
+Metodo de prueba para los calculos 
+
+*/
