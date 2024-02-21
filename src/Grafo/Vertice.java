e6f23b154d2d72804d180462b8ad06f0e9d2cc72
Yangel275 <martinez.yangel@correo.unimet.edu.ve>
2024-02-20 21:26:42 -0400

    Ajustes a la carpeta de Grafo, colocar archivos actualizados

---
+++ src/Grafo/Vertice.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Grafo;
+
+/**
+ *
+ * @author Carl
+ */
+public class Vertice {
+        String nombre;
+        Lista adyacentes;
+        
+    public Vertice(String nombre){
+    this.nombre = nombre;
+    this.adyacentes = new Lista();
+}            
+    
+}
+
+
