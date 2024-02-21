f1bb017b4eb0fc37eb9cc36a53023ac719b046d0
Yangel275 <martinez.yangel@correo.unimet.edu.ve>
2024-02-20 20:41:33 -0400

    Mejoras Ciudad_Gr

--- src/Interfaz/Crear_Ciu.java
+++ src/Interfaz/Crear_Ciu.java
@@ -27,15 +27,18 @@ public class Crear_Ciu extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        Seleccion_ciu = new javax.swing.JComboBox<>();
-        Crear_ciu = new javax.swing.JButton();
-        Editar_ciu = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        Seleccion_ciu = new javax.swing.JComboBox<>();
+        Editar_ciu = new javax.swing.JButton();
+        Crear_ciu = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -43,37 +46,6 @@ public class Crear_Ciu extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
         jLabel1.setText("Crear ciudad o editar ciudades");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 30, -1, -1));
-
-        Seleccion_ciu.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        Seleccion_ciu.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione ciudad", "Item 2", "Item 3", "Item 4" }));
-        Seleccion_ciu.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Seleccion_ciuActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Seleccion_ciu, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 160, 400, -1));
-
-        Crear_ciu.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        Crear_ciu.setText("Crear nueva ciudad");
-        Crear_ciu.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Crear_ciuActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Crear_ciu, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, -1, -1));
-
-        Editar_ciu.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        Editar_ciu.setText("Editar ciudad");
-        Editar_ciu.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Editar_ciuActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Editar_ciu, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 160, 230, -1));
-
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel2.setText("Número de ciudad:");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, -1, -1));
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jButton1.setText("Continuar");
@@ -88,16 +60,59 @@ public class Crear_Ciu extends javax.swing.JFrame {
         jButton2.setText("Actualizar");
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 500, -1, -1));
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("1");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 260, 50, -1));
-
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         jLabel4.setText("Ciudades Adyacentes");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 230, -1, -1));
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel5.setText("Ciudad adyacente:");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 270, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel6.setText("Distancia");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 310, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel2.setText("Número de ciudad:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 280, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("1");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 260, 50, -1));
+
+        jLabel7.setText("jLabel7");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(970, 270, -1, -1));
+
+        Seleccion_ciu.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        Seleccion_ciu.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione ciudad", "Item 2", "Item 3", "Item 4" }));
+        Seleccion_ciu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Seleccion_ciuActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Seleccion_ciu, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 250, -1));
+
+        Editar_ciu.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        Editar_ciu.setText("Editar ciudad");
+        Editar_ciu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Editar_ciuActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Editar_ciu, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 150, 230, -1));
+
+        Crear_ciu.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        Crear_ciu.setText("Crear nueva ciudad");
+        Crear_ciu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Crear_ciuActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Crear_ciu, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 150, -1, -1));
+
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1030, 570));
 
         pack();
@@ -164,6 +179,9 @@ public class Crear_Ciu extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
