b3043112f01cea49059cbfbc9b7839c1ae71f4b3
Yangel275 <martinez.yangel@correo.unimet.edu.ve>
2024-02-23 22:02:09 -0400

    Update Comprobación.java

--- src/Iniciador/Comprobación.java
+++ src/Iniciador/Comprobación.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Iniciador;
+import Calculos.Feoromonas;
+
 
 /**
  *
@@ -14,7 +16,48 @@ public class Comprobación {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* Ciudad 1
+        *   Aristas
+        *       - (Ciudad de inicio: 1, Ciudad de llegada: 2, Distancia: 5)
+        *       - (Ciudad de inicio: 1, Ciudad de llegada: 3, Distancia:  3.1)
+        *       - (Ciudad de inicio: 1, Ciudad de llegada: 6, Distancia:  5.2)
+        */
+        
+        
+        
+        /*
+        * Ciudad 2
+        *       - (Ciudad de inicio: 2, Ciudad de llegada: 7, Distancia: 5.2)
+        */
+        
+        /*
+        * Ciudad 3
+        *       - (Ciudad de inicio: 3, Ciudad de llegada: 2, Distancia: 4,9)
+        *       - (Ciudad de inicio: 3, Ciudad de llegada: 7, Distancia: 3)
+        *       - (Ciudad de inicio: 3, Ciudad de llegada: 5, Distancia: 6)
+        */
+        
+        /*
+        * Ciudad 4
+        */
+        
+        /*
+        * Ciudad 5
+        *       - (Ciudad de inicio: 5, Ciudad de llegada: 4, Distancia: 5,5)
+        */
+        
+        /*
+        * Ciudad 6 
+        *       - (Ciudad de inicio: 6, Ciudad de llegada: 3, Distancia: 3,2)
+        *       - (Ciudad de inicio: 6, Ciudad de llegada: 5, Distancia: 4,7)
+        */
+        
+        /*
+        * Ciudad 7
+        *       - (Ciudad de inicio: 7, Ciudad de llegada: 4, Distancia: 4,8)
+        */
+        
+        
     }
     
 }
